078f10f66c8eca302ab2409a2864fd5bcd5456fb
Daft <daftdevs@gmail.com>
2022-11-19 00:05:43 +0700

    ref: add route for connect, send msg, get msg history

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   mount ActionCable.server => "/cable"
+    post "/create_message", to: "message#create_message"
+    get "/message_history", to: "message#message_history"
+    post "/connect", to: "session#create"
+
 
 end
