18ede07bd8c378f1c49d3c25fc89a65dbc10fabc
Daft <daftdevs@gmail.com>
2022-11-20 20:45:21 +0700

    ref: fix create user bug

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,12 @@
 class SessionController < ApplicationController
     def create
         user = User.find_by(user_id: params[:user_id])
+
         if (user == nil)
-          create_user = User.create(params)
+          create_user = User.create(user_id: params[:user_id])
           render json: create_user, status: :created
         else
-          render json: user
+          render json: user, status: :ok
         end
       end
 end
